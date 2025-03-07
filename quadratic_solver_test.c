9f18e145ff67bb5bbaef2d652f52b595f9bdc2e1
Артемий <freedomskyleva@gmail.com>
2025-03-08 01:18:19 +0600

    fix: all tests in quadratic_solver_test

--- quadratic_solver_test.c
+++ quadratic_solver_test.c
@@ -6,14 +6,14 @@
 
 void test_a_zero() {
   int root_count;
-  double *roots = quadratic_solver(0, 1, 1, &root_count);
+  double *roots = quadratic_solver(0, 1, 1, 1E-14, &root_count);
   assert(root_count == -1);
   assert(roots == NULL);
 }
 
 void test_two_root() {
   int root_count;
-  double *roots = quadratic_solver(1, 0, -1, &root_count);
+  double *roots = quadratic_solver(1, 0, -1, 1E-14, &root_count);
   assert(root_count == 2);
   assert(roots[0] == -1);
   assert(roots[1] == 1);
@@ -21,39 +21,41 @@ void test_two_root() {
 
 void test_one_root() {
   int root_count;
-  double *roots = quadratic_solver(1, 0, 0, &root_count);
+  double *roots = quadratic_solver(1, 0, 0, 1E-14, &root_count);
   assert(root_count == 1);
   assert(roots[0] == 0);
 }
 
 void test_zero_root() {
   int root_count;
-  double *roots = quadratic_solver(1, 0, 1, &root_count);
+  double *roots = quadratic_solver(1, 0, 1, 1E-14, &root_count);
   assert(root_count == 0);
 }
 
 void test_one_root_accuracy() {
+  double eps = 1E-7;
+  double e = 1E-4;
   int root_count;
-  double *roots = quadratic_solver(1, 0, -1E-7, &root_count);
+  double *roots = quadratic_solver(1, 0, -1E-7, eps, &root_count);
   assert(root_count == 2);
-  assert(fabs(roots[0] - (-3E-4)) < 1E-4);
-  assert(fabs(roots[1] - (3E-4)) < 1E-4);
+  assert(fabs(roots[0] + 3E-4) < e);
+  assert(fabs(roots[1] - 3E-4) < e);
 }
 
 void test_two_root_accuracy() {
+  double eps = 1E-11;
   int root_count;
-  double *roots = quadratic_solver(1, -1E+10, -1, &root_count);
+  double *roots = quadratic_solver(1, -1E+10, -1, eps, &root_count);
   assert(root_count == 2);
-  assert(fabs(roots[0] - (-1E-10)) < 1E-11);
-  assert(fabs(roots[1] - (1E+10)) < 1E-11);
+  assert(fabs(roots[0] - (-1E-10)) < eps);
+  assert(fabs(roots[1]) - 1E+10 < eps);
 }
 
 void test_three_root_accuracy() {
   int root_count;
-  double *roots = quadratic_solver(1, 0, -1E-8, &root_count);
-  assert(root_count == 2);
-  assert(fabs(roots[0] - (-1E-4)) < 1E-7);
-  assert(fabs(roots[1] - 1E-4) < 1E-7);
+  double *roots = quadratic_solver(1, 0, -1E-8, 1E-7, &root_count);
+  assert(root_count == 1);
+  assert(roots[0] == 0);
 }
 
 int main() {
