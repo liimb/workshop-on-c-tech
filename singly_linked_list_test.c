3dd1821718d9bf87411a30069b5702aed09db702
Артемий <freedomskyleva@gmail.com>
2025-03-11 14:04:17 +0600

    test: add free memory

--- singly_linked_list_test.c
+++ singly_linked_list_test.c
@@ -7,33 +7,8 @@ void list_create_test() {
   node *list = list_create(1234);
   assert(list->value == 1234);
   assert(list->next == NULL);
-}
 
-void list_add_end_test() {
-  node *list = list_create(5);
-  list_add_end(&list, 6);
-  list_add_end(&list, 7);
-  list_add_end(&list, 10);
-
-  assert(list_get_at(&list, 2)->value == 7);
-}
-
-void list_add_start_test() {
-  node *list = list_create(5);
-  list_add_start(&list, 6);
-  list_add_start(&list, 7);
-  list_add_start(&list, 10);
-
-  assert(list_get_at(&list, 0)->value == 10);
-}
-
-void list_add_middle_test() {
-  node *list = list_create(5);
-  list_add_middle(&list, 6);
-  list_add_middle(&list, 7);
-  list_add_middle(&list, 10);
-
-  assert(list_get_at(&list, 1)->value == 10);
+  list_free(list);
 }
 
 void list_add_at_test() {
@@ -44,6 +19,8 @@ void list_add_at_test() {
   list_add_at(&list, 1, 3);
 
   assert(list_get_at(&list, 1)->value == 3);
+
+  list_free(list);
 }
 
 void list_get_at_test() {
@@ -54,6 +31,8 @@ void list_get_at_test() {
   assert(list_get_at(&list, -1) == NULL);
   assert(list_get_at(&list, 100) == NULL);
   assert(list_get_at(&list, 1)->value == 30);
+
+  list_free(list);
 }
 
 void list_delete_at_test() {
@@ -68,6 +47,8 @@ void list_delete_at_test() {
 
   assert(list_length(list) == 3);
   assert(list_get_at(&list, 2)->value == 10);
+
+  list_free(list);
 }
 
 void list_length_test() {
@@ -82,13 +63,12 @@ void list_length_test() {
   list_add_at(&list, 3, 4);
 
   assert(list_length(list) == 5);
+
+  list_free(list);
 }
 
 int main() {
   list_create_test();
-  list_add_end_test();
-  list_add_start_test();
-  list_add_middle_test();
   list_add_at_test();
   list_get_at_test();
   list_delete_at_test();
