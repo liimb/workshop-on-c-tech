b4200a761e8e1bf5c80621b57ebdbed9d95d3f6b
Артемий <freedomskyleva@gmail.com>
2025-04-15 02:20:46 +0600

    feat: add hash_func

--- hash_table.c
+++ hash_table.c
@@ -1,13 +1,22 @@
 #include "hash_table.h"
 #include "pool_allocator.h"
 
-void hashtable_init(hash_table* table, size_t capacity, pool_allocator* allocator){
-    table->allocator = allocator;
-    table->capacity = capacity;
+size_t hash_func(const char *key, size_t capacity) {
+  size_t hash = 0;
+  while (*key) {
+    hash++;
+  }
+  return hash % capacity;
+}
 
-    allocator = pool_init(sizeof(hashtable_item), capacity);
+void hashtable_init(hash_table *table, size_t capacity,
+                    pool_allocator *allocator) {
+  table->allocator = allocator;
+  table->capacity = capacity;
 
-    for(size_t i = 0; i < capacity; i++) {
-        table->items[i] = (hashtable_item*)pool_alloc(allocator);
-    }
+  allocator = pool_init(sizeof(hashtable_item), capacity);
+
+  for (size_t i = 0; i < capacity; i++) {
+    table->items[i] = (hashtable_item *)pool_alloc(allocator);
+  }
 }
--- hash_table.h
+++ hash_table.h
@@ -1,23 +1,23 @@
-#include <stdio.h>
 #include "pool_allocator.h"
+#include <stdio.h>
 
-typedef struct hashtable_item
-{
-    char* key;
-    void* value;
-    int is_occupied;
-    int is_deleted;
+typedef struct hashtable_item {
+  char *key;
+  void *value;
+  int is_occupied;
+  int is_deleted;
 } hashtable_item;
 
-typedef struct hash_table
-{
-    hashtable_item** items;
-    size_t capacity;
-    pool_allocator* allocator;
+typedef struct hash_table {
+  hashtable_item **items;
+  size_t capacity;
+  pool_allocator *allocator;
 } hash_table;
 
-void hashtable_init(hash_table* table, size_t capacity, pool_allocator* allocator);
-void hashtable_insert(hash_table* table, const char* key, void* value);
-void* hashtable_get(hash_table* table, const char* key);
-void hashtable_del(hash_table* table, const char* key);
-void hashtable_free(hash_table* table);
+size_t hash_func(const char *key, size_t capacity);
+void hashtable_init(hash_table *table, size_t capacity,
+                    pool_allocator *allocator);
+void hashtable_insert(hash_table *table, const char *key, void *value);
+void *hashtable_get(hash_table *table, const char *key);
+void hashtable_del(hash_table *table, const char *key);
+void hashtable_free(hash_table *table);
