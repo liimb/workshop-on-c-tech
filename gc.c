5417554390c0b066ceb64830d1e1e0beae120026
liimb <freedomskyleva@gmail.com>
2025-06-04 01:47:36 +0600

    feat: add cyclic links check

--- gc.c
+++ gc.c
@@ -1,19 +1,20 @@
 #include "gc.h"
+#include <stdbool.h>
 
 void default_constructor(void *object) { (void)object; }
 
 void default_destructor(void *object) { (void)object; }
 
-int ref_count_create(ref_count_t **out_ref, void *object, ref_context ctx) {
+int ref_count_create(ref_count_t **out_ref, void *object, ref_context *ctx) {
   return ref_count_create_ext(out_ref, object, ctx, NULL, NULL);
 }
 
-int ref_count_create_ext(ref_count_t **out_ref, void *object, ref_context ctx,
+int ref_count_create_ext(ref_count_t **out_ref, void *object, ref_context *ctx,
                          ctor constructor, dtor destructor) {
-  if (!out_ref || !ctx.alloc)
+  if (!out_ref || !ctx->alloc)
     return ERROR;
 
-  ref_count_t *ref = ctx.alloc(ctx.mem_context);
+  ref_count_t *ref = ctx->alloc(ctx->mem_context);
   if (!ref)
     return ERROR;
 
@@ -26,6 +27,9 @@ int ref_count_create_ext(ref_count_t **out_ref, void *object, ref_context ctx,
 
   if (ref->constructor && object)
     ref->constructor(object);
+
+  ref->next_global = ctx->all_refs_head;
+  ctx->all_refs_head = ref;
 
   *out_ref = ref;
   return SUCCESS;
@@ -43,7 +47,7 @@ int ref_count_add_dependency(ref_count_t *parent, ref_count_t *child) {
   if (!parent || !child)
     return ERROR;
 
-  dependent_ref *dep = parent->ref_ctx.alloc(parent->ref_ctx.mem_context);
+  dependent_ref *dep = parent->ref_ctx->alloc(parent->ref_ctx->mem_context);
 
   if (!dep)
     return ERROR;
@@ -69,13 +73,13 @@ int ref_count_release(ref_count_t *ref) {
     while (dep) {
       dependent_ref *next = dep->next;
       ref_count_release(dep->ref);
-      ref->ref_ctx.dealloc(ref->ref_ctx.mem_context, dep);
+      ref->ref_ctx->dealloc(ref->ref_ctx->mem_context, dep);
       dep = next;
     }
     ref->dependencies = NULL;
 
-    void *mem_ctx = ref->ref_ctx.mem_context;
-    void (*dealloc)(void *, void *) = ref->ref_ctx.dealloc;
+    void *mem_ctx = ref->ref_ctx->mem_context;
+    void (*dealloc)(void *, void *) = ref->ref_ctx->dealloc;
 
     if (ref->object) {
       ref->destructor(ref->object);
@@ -85,4 +89,59 @@ int ref_count_release(ref_count_t *ref) {
   }
 
   return SUCCESS;
+}
+
+void gc_mark(ref_count_t *ref) {
+  if (!ref || ref->marked)
+    return;
+
+  ref->marked = 1;
+
+  for (dependent_ref *dep = ref->dependencies; dep; dep = dep->next) {
+    gc_mark(dep->ref);
+  }
+}
+
+void gc_collect(ref_context *ctx) {
+  if (!ctx)
+    return;
+
+  for (ref_count_t *ref = ctx->all_refs_head; ref; ref = ref->next_global) {
+    ref->marked = 0;
+  }
+
+  for (ref_count_t *ref = ctx->all_refs_head; ref; ref = ref->next_global) {
+    bool externally_referenced = true;
+    for (ref_count_t *other = ctx->all_refs_head; other;
+         other = other->next_global) {
+      if (other == ref)
+        continue;
+      for (dependent_ref *dep = other->dependencies; dep; dep = dep->next) {
+        if (dep->ref == ref) {
+          externally_referenced = false;
+          break;
+        }
+      }
+      if (!externally_referenced)
+        break;
+    }
+
+    if (externally_referenced && ref->count > 0) {
+      gc_mark(ref);
+    }
+  }
+
+  ref_count_t **prev = &ctx->all_refs_head;
+  ref_count_t *cur = ctx->all_refs_head;
+  while (cur) {
+    ref_count_t *next = cur->next_global;
+    if (!cur->marked) {
+      cur->count = 1;
+      ref_count_release(cur);
+      *prev = next;
+    } else {
+      prev = &cur->next_global;
+    }
+    cur = next;
+  }
 }
